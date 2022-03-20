8653875e7e0c5f32e433d539964b53a1383172af
clueless-skywatcher <somichat@gmail.com>
2022-03-20 15:25:39 +0530

    Added Pow calculations; Updated addition display

--- kcv2/sym/expression.py
+++ kcv2/sym/expression.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from copy import copy
 import functools
 import math
 
@@ -11,6 +12,15 @@ def koolculatorize(expr):
         return Numeric(expr)
     else:
         raise ValueError(f"Cannot convert {expr} to Expression")
+
+def _is_negative(expr):
+    _, numeric = _separate_numeric_from_mul(expr)
+    return numeric.n < 0
+
+def _make_positive(expr):
+    if _is_negative(expr):
+        return expr * Numeric(-1)
+    return expr
 
 def _separate_numeric_from_mul(mul):
     if isinstance(mul, Mul):
@@ -28,9 +38,9 @@ def _separate_numeric_from_mul(mul):
         else:
             numeric_prod = 1
         if len(vars) == 1:
-            return vars[0], Numeric(numeric_prod)
-        return Mul(*vars), Numeric(numeric_prod)
-    elif isinstance(mul, Var):
+            return vars[0], koolculatorize(numeric_prod)
+        return Mul(*vars), koolculatorize(numeric_prod)
+    elif isinstance(mul, (Var, Pow)):
         return mul, Numeric(1)
     else:
         return None, Numeric(1)
@@ -63,7 +73,7 @@ class Expression(ABC):
         return self.__add__(koolculatorize(other))
 
     def __sub__(self, other):
-        return Sub(self, koolculatorize(other))
+        return Sum(self, -koolculatorize(other))
 
     def __mul__(self, other):
         if koolculatorize(other) == Numeric(0):
@@ -71,6 +81,9 @@ class Expression(ABC):
         if koolculatorize(other) == Numeric(1):
             return self
         return Mul(self, koolculatorize(other))
+
+    def __neg__(self):
+        return Numeric(-1) * self
 
     def __rmul__(self, other):
         return self.__mul__(other)
@@ -117,14 +130,28 @@ class Numeric(Expression):
         return hash(self.n)
 
     def __add__(self, other):
+        other = koolculatorize(other)
         if isinstance(other, Numeric):
             return Numeric(self.n + other.n)
         return Sum(self, other)
 
     def __mul__(self, other):
+        other = koolculatorize(other)
         if isinstance(other, Numeric):
             return Numeric(self.n * other.n)
         return Mul(self, koolculatorize(other))
+
+    def __pow__(self, other):
+        other = koolculatorize(other)
+        if isinstance(other, Numeric):
+            return Numeric(self.n ** other.n)
+        return Pow(self, koolculatorize(other))
+
+    def __rpow__(self, other):
+        other = koolculatorize(other)
+        if isinstance(other, Numeric):
+            return Numeric(other.n ** self.n)
+        return Pow(other, self)
 
 class RationalNumber(Numeric):
     pass
@@ -162,7 +189,11 @@ class Sum(Expression):
         self.exps = exps
         self.exps = self._flatten_sums(self.exps)
         self.exps = self._arrange_exps(self.exps)
-    
+        self.exps = self._remove_zeros(self.exps)
+
+    def _remove_zeros(self, exps):
+        return [exp for exp in exps if koolculatorize(exp) != Numeric(0)]
+
     @staticmethod
     def _flatten_sums(exps):
         new_exps = []
@@ -179,8 +210,12 @@ class Sum(Expression):
     @staticmethod
     def _arrange_exps(exps):
         numerics = [x for x in exps if isinstance(x, Numeric)]
-        mul_dict = {_separate_numeric_from_mul(x)[0]: 0 for x in exps if not isinstance(koolculatorize(x), Numeric)}
         
+        mul_dict = {}
+        for exp in exps:
+            if not isinstance(koolculatorize(exp), Numeric):
+                mul, _ = _separate_numeric_from_mul(exp)
+                mul_dict[mul] = mul_dict.get(mul, 0)        
         numeric_sum = sum(numerics)       
 
         for exp in exps:
@@ -188,14 +223,29 @@ class Sum(Expression):
                 mul, num = _separate_numeric_from_mul(exp)
                 if mul is not None:
                     mul_dict[mul] += koolculatorize(num)
-        
-        return [numeric_sum, *[mul * num for mul, num in mul_dict.items()]]
+        final_list = [numeric_sum, *[mul * num for mul, num in mul_dict.items() if koolculatorize(num) != Numeric(0)]]
+        if len(final_list) == 0:
+            final_list = [Numeric(0)]
+        return final_list
 
     def evaluate(self, **bindings):
         return sum([exp.evaluate(**bindings) for exp in self.exps])
 
+    def alternate_display(self):
+        first_exp = self.exps[0]
+        display_str = f"({str(first_exp)})" if isinstance(first_exp, Sum) else str(first_exp)
+        for i in range(1, len(self.exps)):
+            if _is_negative(self.exps[i]):
+                display_str += f" - {str(_make_positive(self.exps[i]))}"
+            else:
+                display_str += f" + {str(self.exps[i])}"
+        return display_str
+
     def display(self):
-        return " + ".join([f"({str(exp)})" if isinstance(exp, (Sum)) else str(exp) for exp in self.exps])
+        if len(self.exps) > 0:
+            return self.alternate_display()
+        else:
+            return "0"
         
     def __hash__(self) -> int:
         return hash(tuple(self.exps))
@@ -210,16 +260,29 @@ class Mul(Expression):
         self.exps = exps
         self.exps = self._flatten_muls(self.exps)
         self.exps = self._arrange_exps(self.exps)
+        self.exps = self._remove_ones(self.exps)
+        self.exps = self._evaluate_zero(self.exps)
+
+    @staticmethod
+    def _evaluate_zero(exps):
+        return [Numeric(0)] if Numeric(0) in [koolculatorize(exp) for exp in exps] else exps
+
+    @staticmethod
+    def _remove_ones(exps):
+        return [exp for exp in exps if koolculatorize(exp) != Numeric(1)]
 
     def srepr_display(self):
-        return f"Mul({', '.join([exp.srepr_display() for exp in self.exps])})"
+        return f"Mul({', '.join([koolculatorize(exp).srepr_display() for exp in self.exps])})"
 
     @staticmethod
     def _arrange_exps(exps):
+        exps = [koolculatorize(exp) for exp in exps]
         numerics = [x for x in exps if isinstance(x, Numeric)]
         vars = [x for x in exps if isinstance(x, Var)]
         sums = [x for x in exps if isinstance(x, Sum)]
-        
+        pows = [x for x in exps if isinstance(x, Pow)]
+        others = [x for x in exps if not isinstance(x, (Numeric, Var, Sum, Mul, Pow))]
+
         prod = lambda x, y: x * y
 
         end_list = []
@@ -227,14 +290,34 @@ class Mul(Expression):
         if len(numerics) > 0:
             numeric_prod = functools.reduce(prod, numerics)
             end_list.append(numeric_prod)
+
         if len(vars) > 0:
-            vars = sorted(vars, key = lambda x: x.symbol)
-            end_list.extend(vars)
+            pow_dict = {x: 0 for x in vars}
+            for var in vars:
+                pow_dict[var] += 1
+            
+            var_list = []
+            for var, pow in pow_dict.items():
+                pow = koolculatorize(pow)
+                if pow == Numeric(1):
+                    var_list.append(var)
+                else:
+                    var_list.append(Pow(var, pow))
+
+            def sort_var_key(var):
+                if isinstance(var, Var):
+                    return var.symbol
+                elif isinstance(var, Pow) and isinstance(var.base, Var):
+                    return var.base.symbol
+                return NotImplemented
+
+            var_list = sorted(var_list, key = sort_var_key)
+            end_list.extend(var_list)
 
         end_list.extend(sums)
+        end_list.extend(others)
+        end_list.extend(pows)
         return end_list
-        
-
 
     @staticmethod
     def _flatten_muls(exps):
@@ -254,7 +337,12 @@ class Mul(Expression):
         return functools.reduce(_mul_func, [exp.evaluate(**bindings) for exp in self.exps])
 
     def display(self):
-        return "*".join([f"({str(exp)})" if isinstance(exp, (Mul, Sum)) else str(exp) for exp in self.exps])
+        str_rep = "*".join([f"({str(exp)})" if isinstance(exp, (Mul, Sum)) else str(exp) for exp in self.exps])
+        if Numeric(-1) in self.exps:
+            positive = _make_positive(self)
+            str_rep = "*".join([f"({str(exp)})" if isinstance(exp, (Mul, Sum)) else str(exp) for exp in positive.exps])
+            str_rep = f"-{str_rep}"
+        return str_rep
 
     def __hash__(self) -> int:
         return hash(tuple(self.exps))
@@ -263,6 +351,10 @@ class Mul(Expression):
         if isinstance(__o, Mul):
             return set(self.exps) == set(__o.exps)
         return False
+
+    def __pow__(self, other):
+        other = koolculatorize(other)
+        return Mul(*[exp ** other for exp in self.exps])
 
 class Div(Expression):
     def __init__(self, num, denom) -> None:
@@ -282,16 +374,6 @@ class Negative(Expression):
     def evaluate(self, **bindings):
         return -self.exp.evaluate(**bindings)
 
-class Sub(Expression):
-    def __init__(self, exp1, exp2) -> None:
-        self.exp1 = exp1
-        self.exp2 = exp2
-
-    def evaluate(self, **bindings):
-        return self.exp1.evaluate(**bindings) - self.exp2.evaluate(**bindings)
-
-    def __hash__(self) -> int:
-        return hash(tuple(self.exp1, self.exp2))
 
 class Pow(Expression):
     def __init__(self, base, exp) -> None:
@@ -305,7 +387,7 @@ class Pow(Expression):
         return self.base.evaluate(**bindings) ** self.exp.evaluate(**bindings)
 
     def __hash__(self) -> int:
-        return hash(tuple(self.base, self.exp))
+        return hash(tuple([self.base, self.exp]))
 
     def __mul__(self, other):
         if isinstance(other, Pow):
@@ -317,7 +399,19 @@ class Pow(Expression):
         return Pow(self.base, koolculatorize(self.exp) * koolculatorize(other))
 
     def display(self):
-        return f"{self.base}**{self.exp}"
+        base_rep = self.base.display()
+        exp_rep = self.exp.display()
+        if isinstance(self.base, Sum):
+            base_rep = f"({base_rep})"
+        if isinstance(self.exp, (Sum, Mul)):
+            exp_rep = f"({exp_rep})"
+
+        return f"{base_rep}^{exp_rep}"
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, Pow):
+            return self.base == __o.base and self.exp == __o.exp
+        return False
 
 _FUNC_BINDINGS = {
     "sin": math.sin,
@@ -335,11 +429,17 @@ class ApplyFunction(Expression):
     def evaluate(self, **bindings):
         return _FUNC_BINDINGS[self.func](self.arg.evaluate(**bindings))
 
+    def display(self):
+        return f"{self.__class__.__name__}({str(self.arg)})"
+
+    def srepr_display(self):
+        return f"{self.__class__.__name__}({self.arg.srepr_display()})"
+
 class NumberOfOperands(UtilityFunction):
     def eval(expr):
         if isinstance(expr, (Sum, Mul)):
             return len(expr.exps)
-        elif isinstance(expr, (Div, Sub)):
+        elif isinstance(expr, (Div)):
             return 2
         elif isinstance(expr, (Var, Numeric)):
             return 1
@@ -349,8 +449,6 @@ class NthOperand(UtilityFunction):
         try:
             if isinstance(expr, (Sum, Mul)):
                 return expr.exps[i]
-            elif isinstance(expr, Sub):
-                return [expr.exp1, expr.exp2][i]
             elif isinstance(expr, Div):
                 return [expr.num, expr.denom][i]
             elif isinstance(expr, (Var, Numeric)):
@@ -363,7 +461,6 @@ class NthOperand(UtilityFunction):
 
 class FreeOf(UtilityFunction):
     def eval(expr, free_of):
-        print(f"{expr} == {free_of} -> {expr == free_of}")
         free_of = koolculatorize(free_of)
         if expr == free_of:
             return False
@@ -373,8 +470,6 @@ class FreeOf(UtilityFunction):
             return False 
         if isinstance(expr, (Sum, Mul)):
             return all([FreeOf.eval(exp, free_of) for exp in expr.exps])
-        if isinstance(expr, Sub):
-            return FreeOf.eval(expr.exp1, free_of) and FreeOf.eval(expr.exp2)
         if isinstance(expr, Div):
             return FreeOf.eval(expr.num, free_of) and FreeOf.eval(expr.denom, free_of)
 
